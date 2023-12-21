2c64b930513c48340c2bcc389f6f3e35746320ce
Chng Yi Cheng <42273473+chngyicheng@users.noreply.github.com>
2023-12-21 17:52:27 +0800

    [BUGFIX] Fixed bug where CPU Utilization was calculated wrongly.

--- src/processor.cpp
+++ src/processor.cpp
@@ -7,25 +7,14 @@ float Processor::Utilization() {
   std::vector<std::string> cpuValues = LinuxParser::CpuUtilization();
 
   // Ensure there are enough values to calculate utilization
-  if (cpuValues.size() >= LinuxParser::kSteal_ + 1) {
-    // Calculate the total active and total jiffies
-    float activeJiffies = std::stof(cpuValues[LinuxParser::kUser_]) +
-                          std::stof(cpuValues[LinuxParser::kNice_]) +
-                          std::stof(cpuValues[LinuxParser::kSystem_]) +
-                          std::stof(cpuValues[LinuxParser::kIRQ_]) +
-                          std::stof(cpuValues[LinuxParser::kSoftIRQ_]) +
-                          std::stof(cpuValues[LinuxParser::kSteal_]) +
-                          std::stof(cpuValues[LinuxParser::kGuest_]) +
-                          std::stof(cpuValues[LinuxParser::kGuestNice_]);
+  float totalJiffies = LinuxParser::Jiffies();
 
-    float totalJiffies = activeJiffies +
-                          std::stof(cpuValues[LinuxParser::kIdle_]) +
-                          std::stof(cpuValues[LinuxParser::kIOwait_]);
+  float activeJiffies = LinuxParser::ActiveJiffies();
 
-    // Calculate CPU utilization as a percentage
-    return activeJiffies / totalJiffies;
+  // Calculate CPU utilization as a percentage
+  if (totalJiffies > 0) {    // Calculate the total active and total jiffies
+    return (activeJiffies) / totalJiffies;
   }
-
   // Return 0 if there was an issue getting CPU utilization
   return 0.0;
 }
